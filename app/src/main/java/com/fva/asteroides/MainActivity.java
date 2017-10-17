30294997951e1a7e70fe7988db03150045a35825
Franz Villalpando <villalpando.franz@usfx.bo>
2017-10-17 11:03:33 -0400

    Implementado el boton Acerca de....

--- app/src/main/java/com/fva/asteroides/MainActivity.java
+++ app/src/main/java/com/fva/asteroides/MainActivity.java
@@ -1,14 +1,28 @@
 package com.fva.asteroides;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bAcercaDe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main_con_linearlayout);
         setContentView(R.layout.activity_main);
+        bAcercaDe = (Button) findViewById(R.id.button7);
+        bAcercaDe.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                lanzarAcercaDe(null);
+            }
+        });
+    }
+        public void lanzarAcercaDe(View view) {
+        Intent i = new Intent(this, acercaDeActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/fva/asteroides/acercaDeActivity.java
@@ -0,0 +1,15 @@
+package com.fva.asteroides;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by DTIC-Dir on 17/10/2017.
+ */
+
+public class acercaDeActivity extends Activity {
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acercade);
+    }
+}
