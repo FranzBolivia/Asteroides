16bb47a5609ec8ca10ddbcfea8e80ae96f4c3a56
Franz Villalpando <villalpando.franz@usfx.bo>
2017-10-27 00:50:11 -0400

    Intriducioendo la nave en vista juego

--- app/src/main/java/com/fva/asteroides/VistaJuego.java
+++ app/src/main/java/com/fva/asteroides/VistaJuego.java
@@ -15,15 +15,32 @@ import java.util.List;
  */
 
 public class VistaJuego extends View {
+    // //// NAVE //////
+    private Grafico nave; // Gráfico de la nave
+    private int giroNave; // Incremento de dirección
+    private double aceleracionNave; // aumento de velocidad
+    private static final int MAX_VELOCIDAD_NAVE = 20;
+    // Incremento estándar de giro y aceleración
+    private static final int PASO_GIRO_NAVE = 5;
+    private static final float PASO_ACELERACION_NAVE = 0.5f;
+
     // //// ASTEROIDES //////
     private List<Grafico> asteroides; // Lista con los Asteroides
     private int numAsteroides = 5; // Número inicial de asteroides
     private int numFragmentos = 3; // Fragmentos en que se divide
     public VistaJuego(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+
+
         Drawable drawableNave, drawableAsteroide, drawableMisil;
-        drawableAsteroide =
-                context.getResources().getDrawable(R.drawable.asteroide1);
+
+        drawableNave = context.getResources().getDrawable(R.drawable.nave);
+        ContextCompat.getDrawable(context, R.drawable.nave);
+        nave = new Grafico(this, drawableNave);
+
+
+        drawableAsteroide = context.getResources().getDrawable(R.drawable.asteroide1);
         ContextCompat.getDrawable(context, R.drawable.asteroide1);
         asteroides = new ArrayList<Grafico>();
         for (int i = 0; i < numAsteroides; i++) {
@@ -39,13 +56,19 @@ public class VistaJuego extends View {
                                            int ancho_anter, int alto_anter) {
         super.onSizeChanged(ancho, alto, ancho_anter, alto_anter);
 // Una vez que conocemos nuestro ancho y alto.
+        nave.setCenX((int)ancho/2);
+        nave.setCenY((int)alto/2);
+
         for (Grafico asteroide: asteroides) {
-            asteroide.setCenX((int) (Math.random()*ancho));
-            asteroide.setCenY((int) (Math.random()*alto));
+            do {
+                asteroide.setCenX((int) (Math.random()*ancho));
+                asteroide.setCenY((int) (Math.random()*alto));
+            } while(asteroide.distancia(nave) < (ancho+alto)/5);
         }
     }
     @Override protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        nave.dibujaGrafico(canvas);
         for (Grafico asteroide: asteroides) {
             asteroide.dibujaGrafico(canvas);
         }
